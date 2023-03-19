b85bb8d9d2b7082fb212db56349ee50a2baad0a0
Beybut Abdulragimov <31037730+BeyCoder@users.noreply.github.com>
2023-03-19 20:29:46 +0400

    preparing npm module

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,2 @@
-import { Address } from "ton-core";
-import { Account } from "./blockchain/wallets/Account";
 import { JettonWallet } from "./blockchain/wallets/JettonWallet.js";
 import { TonCatTransactionFetcher } from "./blockchain/transactions/fetchers/TonCatTransactionFetcher.js";
-
-const admin: string = "EQALyrWQNCxR3RFcape5ZMxClDHpySrEO93lQiaM9ZWUnAjW";
-const account: Account = new JettonWallet(Address.parse(admin));
-const fetcher: TonCatTransactionFetcher = new TonCatTransactionFetcher(account);
-
-fetcher.fetchTransactions(10);
