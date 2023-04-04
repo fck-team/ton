330e4ef48f76916337110efad78ffd245fd9fa5f
Beybut Abdulragimov <31037730+BeyCoder@users.noreply.github.com>
2023-04-04 23:04:49 +0400

    tonweb support

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,5 @@ export { Wallet } from "./blockchain/wallets/Wallet.js";
 export { UnknownWallet } from "./blockchain/wallets/UnknownWallet.js";
 export { WalletType } from "./blockchain/wallets/WalletType.js";
 export { TonCatTransactionFetcher } from "./blockchain/transactions/fetchers/TonCatTransactionFetcher.js";
+export { Blockchain } from "./blockchain/Blockchain.js"
+export { BlockchainNetwork } from "./blockchain/BlockchainNetwork.js"
--- src/blockchain/Blockchain.ts
+++ src/blockchain/Blockchain.ts
@@ -1,9 +1,11 @@
 import { TonClient4 } from "ton";
 import { config } from "../config.js";
 import { BlockchainNetwork } from "./BlockchainNetwork.js";
+import TonWeb from "tonweb";
 
 export class Blockchain {
     client: TonClient4
+    client_tonweb: TonWeb
 
     constructor(mode: BlockchainNetwork)
     {
@@ -13,6 +15,9 @@ export class Blockchain {
             this.client = new TonClient4({
                 endpoint: 'https://toncenter.com/api/v2/jsonRPC?api_key=' + api_key,
             });
+            this.client_tonweb = new TonWeb(new TonWeb.HttpProvider('https://toncenter.com/api/v2/jsonRPC', {
+                apiKey: api_key
+            }));
         }
         else if(mode == BlockchainNetwork.testnet)
         {
@@ -20,6 +25,9 @@ export class Blockchain {
             this.client = new TonClient4({
                 endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC?api_key=' + api_key,
             });
+            this.client_tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', {
+                apiKey: api_key
+            }));
         }
     }
 }
