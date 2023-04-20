ae90468a199b422fff42affef25a5a00839d0164
Beybut Abdulragimov <31037730+BeyCoder@users.noreply.github.com>
2023-04-20 23:30:48 +0400

    fix

--- src/blockchain/Blockchain.ts
+++ src/blockchain/Blockchain.ts
@@ -1,32 +1,31 @@
+import TonWeb from "@fck-foundation/tonweb-ts";
 import { TonClient4 } from "ton";
 import { config } from "../config.js";
 import { BlockchainNetwork } from "./BlockchainNetwork.js";
-import TonWeb from "@fck-foundation/tonweb-ts/dist/types";
 
 export class Blockchain {
-    client: TonClient4
-    client_tonweb: TonWeb
+    public client: TonClient4;
+    // tslint:disable-next-line:variable-name
+    public client_tonweb: TonWeb;
 
-    constructor(mode: BlockchainNetwork)
-    {
-        if(mode == BlockchainNetwork.mainnet) 
-        {
+    constructor(mode: BlockchainNetwork) {
+        if (mode === BlockchainNetwork.mainnet) {
+            // tslint:disable-next-line:variable-name
             const api_key = config.MAINNET_API_KEY;
             this.client = new TonClient4({
-                endpoint: 'https://toncenter.com/api/v2/jsonRPC?api_key=' + api_key,
+                endpoint: "https://toncenter.com/api/v2/jsonRPC?api_key=" + api_key,
             });
-            this.client_tonweb = new TonWeb(new TonWeb.HttpProvider('https://toncenter.com/api/v2/jsonRPC', {
-                apiKey: api_key
+            this.client_tonweb = new TonWeb(new TonWeb.HttpProvider("https://toncenter.com/api/v2/jsonRPC", {
+                apiKey: api_key,
             }));
-        }
-        else if(mode == BlockchainNetwork.testnet)
-        {
+        } else if (mode === BlockchainNetwork.testnet) {
+            // tslint:disable-next-line:variable-name
             const api_key = config.TESTNET_API_KEY;
             this.client = new TonClient4({
-                endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC?api_key=' + api_key,
+                endpoint: "https://testnet.toncenter.com/api/v2/jsonRPC?api_key=" + api_key,
             });
-            this.client_tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', {
-                apiKey: api_key
+            this.client_tonweb = new TonWeb(new TonWeb.HttpProvider("https://testnet.toncenter.com/api/v2/jsonRPC", {
+                apiKey: api_key,
             }));
         }
     }
